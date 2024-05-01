721bd2d3925c54a15e349d206168d7397768c764
amirshariati <seyed.amir.shariati@gmail.com>
2024-05-01 19:27:28 +0330

    add imports

--- back/apps/movie/models.py
+++ back/apps/movie/models.py
@@ -1,3 +1,8 @@
+import datetime
+
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
+from django.utils.translation import gettext_lazy as _
+
 
 # Create your models here.
