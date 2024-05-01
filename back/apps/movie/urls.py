4ab6f55928810e6249d969babfb8125f9d2fe376
amirshariati <seyed.amir.shariati@gmail.com>
2024-05-01 19:31:51 +0330

    add urlpatterns

--- back/apps/movie/urls.py
+++ back/apps/movie/urls.py
@@ -1,3 +1,6 @@
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 from .views import MovieListView, MovieRetrieveView
+
+urlpatterns = [
+]
