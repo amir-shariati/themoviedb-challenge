8bb3a1b64a7534d139911a941725e6a448080d96
amirshariati <seyed.amir.shariati@gmail.com>
2024-05-01 19:33:48 +0330

    add MovieResponseType

--- back/apps/movie/tasks.py
+++ back/apps/movie/tasks.py
@@ -10,6 +10,23 @@ from .models import Movie
 logger = get_task_logger(__name__)
 
 
+class MovieResponseType(TypedDict):
+    adult: bool
+    backdrop_path: str
+    # genre_ids: List[int]
+    movie_id: int
+    original_language: str
+    original_title: str
+    overview: str
+    popularity: float
+    poster_path: str
+    release_date: str
+    title: str
+    video: bool
+    vote_average: float
+    vote_count: str
+
+
 @shared_task
 def movie_update_every_two_hours():
     logger.info(f'update weather at : {datetime.now()}')
