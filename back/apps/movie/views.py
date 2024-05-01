22e0fea3d1fdb461394f3abc15a1dcfa22440fea
amirshariati <seyed.amir.shariati@gmail.com>
2024-05-01 19:29:51 +0330

    add imports

--- back/apps/movie/views.py
+++ back/apps/movie/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from rest_framework import generics, views, filters
+from .models import Movie
+from .serializers import MovieSerializer
+
 
 # Create your views here.
