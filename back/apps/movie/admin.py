0071229c60a7a55cced219a0fc0e386f8047d414
amirshariati <seyed.amir.shariati@gmail.com>
2024-05-01 19:28:18 +0330

    add MovieAdmin

--- back/apps/movie/admin.py
+++ back/apps/movie/admin.py
@@ -3,3 +3,10 @@ from .models import Movie
 
 
 # Register your models here.
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'updated_at')
+    search_fields = ('title',)
+    readonly_fields = ('created_at', 'updated_at')
+
+    list_per_page = 25
