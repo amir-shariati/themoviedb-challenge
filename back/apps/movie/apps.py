6cdea3657b9879b2fd1eba9ecd682f4bcdff7cd6
amirshariati <seyed.amir.shariati@gmail.com>
2024-05-01 19:28:51 +0330

    add ready to run one_time_app_startup_task

--- back/apps/movie/apps.py
+++ back/apps/movie/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class MovieConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.movie'
+
+    def ready(self):
+        from .tasks import one_time_app_startup_task
+        if not settings.TASK_START_UP_ALREADY_RUN and settings.DOCKER_BACKEND_SERVER:
+            one_time_app_startup_task.delay()
+            settings.TASK_START_UP_ALREADY_RUN = True
