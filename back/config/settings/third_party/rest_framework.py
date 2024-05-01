bd42e338f09bb36146d87f1faecf7c817b73fb4f
amirshariati <seyed.amir.shariati@gmail.com>
2024-05-01 19:25:24 +0330

    remove comments

--- back/config/settings/third_party/rest_framework.py
+++ back/config/settings/third_party/rest_framework.py
@@ -1,11 +1,5 @@
 
 REST_FRAMEWORK = {
-    # 'DEFAULT_RENDERER_CLASSES': [
-    #     'rest_framework.renderers.JSONRenderer',
-    # ],
-    # 'DEFAULT_PARSER_CLASSES': [
-    #     'rest_framework.parsers.JSONParser',
-    # ],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
     # new
