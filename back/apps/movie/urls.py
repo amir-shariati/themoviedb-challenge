4e77df31707f2dc3454d3bc905cbdbf79cb5b1ce
amirshariati <seyed.amir.shariati@gmail.com>
2024-05-01 19:32:58 +0330

    add swagger and redoc

--- back/apps/movie/urls.py
+++ back/apps/movie/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('movie/', MovieListView.as_view(), name='movie-list'),
     path('movie/<int:pk>', MovieRetrieveView.as_view(), name='movie-detail'),
 
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('schema/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger'),
+    path('schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
